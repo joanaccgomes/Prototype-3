993aefb4a87ffd34a94a3d5cca2b08e84a1120f7
joanaccgomes <a63577@alunos.ipb.pt>
2026-06-11 17:49:25 +0100

    Lesson 3.2 - Make the World Whiz By

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
@@ -7,6 +8,8 @@ public class PlayerController : MonoBehaviour
      public float jumpForce =10;
      public float gravityModifier;
      public bool isOnGround = true;
+
+     public bool gameOver = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,7 +33,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround=true;
+        if (collision.gameObject.CompareTag("Ground")){
+            isOnGround=true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver=true;
+            Debug.Log("Game Over!");
+
+        }
     }
 
 }
